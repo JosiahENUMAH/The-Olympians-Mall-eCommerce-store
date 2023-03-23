c927c5c4009c82353ba3f38afbf53264087708e1
Josiah ENUMAH <josiahenumah94@gmail.com>
2023-03-23 13:29:11 +0100

    updates

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -4,7 +4,7 @@ import React, { createContext, useState } from "react";
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  // cart state
+  //  cart state
   const [cart, setCart] = useState([]);
   return (
     <CartContext.Provider value={"this is the cart context"}>
